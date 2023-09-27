61ca2ea09c18d4231673b7f309325a308b9c4c7c
Eric Todd <roboevt@gmail.com>
2023-09-27 17:23:00 -0500

    Worked on classification

--- cluster_covid_data.m
+++ cluster_covid_data.m
@@ -1,1 +1,22 @@
-%eparates the data into "training" and “testing” groups, uses kmeans clustering on the “training” group, and results in the construction of k centroids
+%eparates the data into "training" and “testing” groups, uses kmeans clustering 
+% on the “training” group, and results in the construction of k centroids
+
+close all
+load('COVIDbyCounty.mat')
+
+[idx, C] = kmeans(CNTY_COVID, 9);
+
+CNTY_CENSUS.DIVISION(idx==2)
+
+test = movmean(CNTY_COVID, 50);
+
+% Prototype idea for determinging optimal k and w.
+for w=5:50
+    for k=9:20
+        data = movmean(CNTY_COVID, w)
+        [idx, C, sumd] = kmeans(data, k);
+        error[w,k] = cost(sumd)
+    end
+end
+
+plot(test')
